e56237e5ce5f17c2d8bb014c5513e5e7c6a6306a
Miguelzy200 <miguelzy748@gmail.com>
2024-05-14 11:36:29 -0300

    Print em funções assíncronas

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import asyncio
+
+async def print1():
+    print("en: Hello World!")
+
+async def print2():
+    print("pt-br: Olá Mundo!")
+
+async def main():
+    task1 = asyncio.create_task(print1())
+    task2 = asyncio.create_task(print2())
+
+    await task1
+    await task2
+
+asyncio.run(main())
